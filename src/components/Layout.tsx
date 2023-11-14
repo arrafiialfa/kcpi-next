26f90b773f96d94655589c34bb6e9981d32cd1e8
arrafii <arrafiialfa.m@gmail.com>
2023-11-14 21:26:18 +0700

    fix layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,18 +3,13 @@ import Footer from "./common/Footer";
 
 export default function Layout({
   children,
-  useBreadCrumb,
 }: Readonly<{
   children: React.ReactNode;
-  useBreadCrumb: boolean;
 }>) {
   return (
     <>
       <Navbar />
-      <div>
-        {useBreadCrumb && <div>breadcrumbs</div>}
-        {children}
-      </div>
+      <div>{children}</div>
       <Footer />
     </>
   );
