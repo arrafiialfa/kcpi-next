8eb62f949554cebf4cfc6f79d4ab3efac0d62721
arrafii <arrafiialfa.m@gmail.com>
2023-11-15 16:41:40 +0700

    fix nav

--- src/components/ui/navbar/MenuDropdown.tsx
+++ src/components/ui/navbar/MenuDropdown.tsx
@@ -25,58 +25,60 @@ export default function MenuDropdown({
   const closeMenu = () => setIsMenuOpen(false);
 
   return (
-    <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
-      <MenuHandler>
-        <Button
-          variant="text"
-          color="blue-gray"
-          className="flex items-center gap-1 rounded-full py-0.5 px-2 lg:ml-auto"
-        >
-          {children}
-          {menuList && (
-            <ChevronDownIcon
-              strokeWidth={2.5}
-              className={`h-3 w-3 transition-transform ${
-                isMenuOpen ? "rotate-180" : ""
-              }`}
-            />
-          )}
-        </Button>
-      </MenuHandler>
-      {menuList && (
-        <MenuList className="p-1">
-          {menuList.map(({ label, icon, route }, key) => {
-            return (
-              <MenuItem
-                key={label}
-                onClick={() => {
-                  if (route) {
-                    router.push(route);
-                  }
-                  closeMenu();
-                }}
-                className={`flex items-center gap-2`}
-              >
-                <Link key={label} href={route ?? "#"}>
-                  {icon &&
-                    React.createElement(icon, {
-                      className: `h-4 w-4`,
-                      strokeWidth: 2,
-                    })}
-                  <Typography
-                    as="span"
-                    variant="small"
-                    className="font-normal"
-                    color={"inherit"}
-                  >
-                    {label}
-                  </Typography>
-                </Link>
-              </MenuItem>
-            );
-          })}
-        </MenuList>
-      )}
-    </Menu>
+    <li>
+      <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
+        <MenuHandler>
+          <Button
+            variant="text"
+            color="blue-gray"
+            className="flex items-center gap-1 rounded-full py-0.5 px-2 lg:ml-auto"
+          >
+            {children}
+            {menuList && (
+              <ChevronDownIcon
+                strokeWidth={2.5}
+                className={`h-3 w-3 transition-transform ${
+                  isMenuOpen ? "rotate-180" : ""
+                }`}
+              />
+            )}
+          </Button>
+        </MenuHandler>
+        {menuList && (
+          <MenuList className="p-1">
+            {menuList.map(({ label, icon, route }, key) => {
+              return (
+                <MenuItem
+                  key={label}
+                  onClick={() => {
+                    if (route) {
+                      router.push(route);
+                    }
+                    closeMenu();
+                  }}
+                  className={`flex items-center gap-2`}
+                >
+                  <Link key={label} href={route ?? "#"}>
+                    {icon &&
+                      React.createElement(icon, {
+                        className: `h-4 w-4`,
+                        strokeWidth: 2,
+                      })}
+                    <Typography
+                      as="span"
+                      variant="small"
+                      className="font-normal"
+                      color={"inherit"}
+                    >
+                      {label}
+                    </Typography>
+                  </Link>
+                </MenuItem>
+              );
+            })}
+          </MenuList>
+        )}
+      </Menu>
+    </li>
   );
 }
