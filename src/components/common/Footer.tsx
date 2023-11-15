431b72904081220a74133f279bf89317a14ef7f1
arrafii <arrafiialfa.m@gmail.com>
2023-11-15 09:02:53 +0700

    centered footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="w-full justify-center bg-gray-800 p-12">
-      <p className="text-gray-400 font-light text-sm">
+      <p className="text-gray-400 font-light text-sm text-center">
         Copyright 2017. Direktorat Jenderal Pengendalian Perubahan Iklim -
         Kementerian Lingkungan Hidup dan Kehutanan
       </p>
